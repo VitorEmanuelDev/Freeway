85adb9fd65aae30d409dce9bf514caf2d839cb8b
Vitor Emanuel <veslima3@gmail.com>
2019-09-09 18:30:53 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,22 @@
+//road variables
 let roadImage;
+
+//player variables
 let yourImage;
+
+let yPlayer = 370;
+let xPlayer = 70;
+
+//car variables
 let carImageOne;
+let xCar = 700;
+let yCar = 40;
+
 
 function preload(){
-  roadImage = loadImage("Assets/road.png");
-  yourImage = loadImage("Assets/you.png");  
-  carImageOne = loadImage("Assets/car1.png");
+  roadImage = loadImage("road.png");
+  yourImage = loadImage("you.png");  
+  carImageOne = loadImage("car1.png");
 }
 
 
@@ -15,7 +26,35 @@ function setup() {
 
 function draw() {
   background(roadImage);
-  image(yourImage, 200, 370, 30, 25)
-  image(carImageOne,420, 40, 80,40);
-  
+  showPlayer();
+  showCars();
+  moveCar();
+  movePlayer();
+}
+
+function showPlayer() {
+  image(yourImage, xPlayer,yPlayer, 30, 25);
+}
+
+function showCars(){
+  image(carImageOne,xCar, yCar, 80,40);
+}
+
+function moveCar(){
+xCar -= 2;
+}
+
+function movePlayer(){
+if (keyIsDown (UP_ARROW)){
+  yPlayer -= 3;
+  }
+if (keyIsDown (DOWN_ARROW)){
+  yPlayer += 3;
+}
+if (keyIsDown (RIGHT_ARROW)){
+  xPlayer += 3;
+}
+if (keyIsDown (LEFT_ARROW)){
+  xPlayer -= 3;
+}
 }
