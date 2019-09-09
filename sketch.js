0f7aa2c238b842c0c8addc8475351e99e24dfc00
Vitor Emanuel <veslima3@gmail.com>
2019-09-09 13:22:35 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,21 @@
 let roadImage;
+let yourImage;
+let carImageOne;
 
 function preload(){
-  roadImage = loadImage("Assets/road.png")
+  roadImage = loadImage("Assets/road.png");
+  yourImage = loadImage("Assets/you.png");  
+  carImageOne = loadImage("Assets/car1.png");
 }
 
 
 function setup() {
   createCanvas(500, 400);
-}
+} 
 
 function draw() {
   background(roadImage);
+  image(yourImage, 200, 370, 30, 25)
+  image(carImageOne,420, 40, 80,40);
   
 }
