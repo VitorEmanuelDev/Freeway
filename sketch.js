761e6717c5820f63003198f1e1fd48e3693e3b23
Vitor Emanuel <veslima3@gmail.com>
2019-09-09 12:03:03 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,15 @@
+let roadImage;
+
+function preload(){
+  roadImage = loadImage("road.png")
+}
+
+
+function setup() {
+  createCanvas(500, 400);
+}
+
+function draw() {
+  background(roadImage);
+  
+}
